532590d5dc6905b884b5b4255c68281db9ad13c5
RedMoikle <moikle@live.co.uk>
2021-05-28 16:36:21 +0100

    Create GameManager class

--- main.py
+++ main.py
@@ -11,11 +11,51 @@ from game_pieces import *
 
 
 def run():
-    BoardTile()
-    BoardTile(position=(1, 0, 0))
-    BoardTile(position=(0, 0, 1))
-    return None
+    manager = GameManager()
+    return manager
+
+
+class GameManager(object):
+    def __init__(self):
+        self.pieces = {}
+        self.scriptjob = None
+        self.create_board()
+        self.create_pieces()
+        self.create_scriptjob()
+    def __del__(self):
+        self.delete_scriptjob()
+
+    def add_piece(self, piece):
+        self.pieces[piece.transform] = piece
+
+    def create_scriptjob(self):
+        self.scriptjob = pm.scriptJob(conditionTrue=['SelectionChanged', self._selection_made], protected=True)
+    def delete_scriptjob(self):
+        if self.scriptjob is None:
+            return
+        print("kill sj")
+        pm.scriptJob(kill=self.scriptjob, force=True)
+    def _selection_made(self):
+        print("sel")
+        sel = pm.selected()
+        if not sel:
+            return
+        if sel[0] not in self.pieces:
+            return
+        gameobject = self.pieces[sel[0]]
+        if isinstance(gameobject, Interactable):
+            gameobject.action()
+            pm.select(clear=True)
+
+
+    def create_board(self):
+        BoardTile(self)
+        BoardTile(self, position=(1, 0, 0))
+        BoardTile(self, position=(0, 0, 1))
+
+    def create_pieces(self):
+        Interactable(self)
 
 
 if __name__ == '__main__':
-    run()
+    ur_manager = run()
